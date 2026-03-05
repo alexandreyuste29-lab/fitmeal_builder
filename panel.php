297012ffacfb91c9afd9ab01293b24e7ea48a03e
Alex <alexandreyuste29-lab@gmail.com>
2026-03-05 19:17:51 +0100

    Panel.php

---
+++ panel.php
@@ -0,0 +1,19 @@
+<?php 
+
+session_start();
+
+//Comprobar si el usuario ha iniciado sesión. Si no, redirige al login
+
+if(!isset($_SESSION['id_usuario'])){
+    header("Location: login.html");
+    exit();
+}
+
+//Guardar nombre del usuaario en variable para el Html
+
+$nombreUsuario = $_SESSION['nombre'];
+
+//Llamada al Html
+
+include("Panel.html");
+?>
