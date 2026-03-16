c0a313439f2e73981aeb9676760ac1c86a56029e
Alex <alexandreyuste29-lab@gmail.com>
2026-03-16 17:46:33 +0100

    Ver tupper

---
+++ ver_tupper.php
@@ -0,0 +1,50 @@
+<?php 
+
+session_start();
+
+//Comprobamos si el usuario ha iniciado sesión
+
+if(!isset($_SESSION['id_usuario'])){
+    header("Location: login.html");
+    exit();
+}
+
+$nombreUsuario = $_SESSION['nombre'];
+
+include("includes/conexion.php");
+
+//Comprobamos si llega el id del tupper
+
+if(!isset($_GET['id_tupper'])){
+    die("Tupper no encontrado.");
+}
+
+$idTupper = (int)$_GET['id_tupper'];
+
+//Consulta para obtener los alimentos del tupper
+
+$sql = "SELECT alimentos.nombre FROM tuppers_alimentos JOIN alimentos 
+ON tuppers_alimentos.id_alimento =alimentos.id_alimento WHERE tuppers_alimentos.id_tupper = $idTupper";
+
+$resultado = $conexion->query($sql);
+
+//Construimos la lista
+
+$listaAlimentos="";
+
+if($resultado->num_rows>0){
+
+$listaAlimentos .= "<ul>";
+
+while($fila = $resultado->fetch_assoc()){
+    $nombre = $fila['nombre'];
+    $listaAlimentos .= "<li>$nombre</li>";
+}
+
+$listaAlimentos .= "</ul>";
+
+}else{$listaAlimentos = "<p>Este tupper no tiene alimentos todavía.</p>";}
+
+include("ver_tupper.html");
+
+?>
