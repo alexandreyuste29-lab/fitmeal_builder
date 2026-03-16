9e677b6331c1717a891328e351c19526fb44e5b2
Alex <alexandreyuste29-lab@gmail.com>
2026-03-16 18:19:18 +0100

    Ver Tupper + calorias

--- ver_tupper.php
+++ ver_tupper.php
@@ -8,7 +8,6 @@ if(!isset($_SESSION['id_usuario'])){
     header("Location: login.html");
     exit();
 }
-
 $nombreUsuario = $_SESSION['nombre'];
 
 include("includes/conexion.php");
@@ -23,14 +22,17 @@ $idTupper = (int)$_GET['id_tupper'];
 
 //Consulta para obtener los alimentos del tupper
 
-$sql = "SELECT alimentos.nombre FROM tuppers_alimentos JOIN alimentos 
-ON tuppers_alimentos.id_alimento =alimentos.id_alimento WHERE tuppers_alimentos.id_tupper = $idTupper";
+$sql = "SELECT alimentos.nombre, alimentos.calorias
+FROM tuppers_alimentos JOIN alimentos 
+ON tuppers_alimentos.id_alimento =alimentos.id_alimento 
+WHERE tuppers_alimentos.id_tupper = $idTupper";
 
 $resultado = $conexion->query($sql);
 
 //Construimos la lista
 
 $listaAlimentos="";
+$totalCalorias = 0;
 
 if($resultado->num_rows>0){
 
@@ -38,10 +40,13 @@ $listaAlimentos .= "<ul>";
 
 while($fila = $resultado->fetch_assoc()){
     $nombre = $fila['nombre'];
-    $listaAlimentos .= "<li>$nombre</li>";
+    $calorias = $fila['calorias'];
+    $totalCalorias += $calorias;
+    $listaAlimentos .= "<li>$nombre - $calorias kcal</li>";
 }
 
 $listaAlimentos .= "</ul>";
+$listaAlimentos .= "<p><strong>Calorías totales del tupper: $totalCalorias kcal </strong></p>";
 
 }else{$listaAlimentos = "<p>Este tupper no tiene alimentos todavía.</p>";}
 
